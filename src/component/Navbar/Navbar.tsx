95a27e09ee2f61886c29faa1662474870265691c
Aniket Tamrakar <anikettamrakar0@gmail.com>
2023-08-29 23:34:32 +0545

    Installed React-Query and wrapped in App.tsx

---
+++ src/component/Navbar/Navbar.tsx
@@ -0,0 +1,12 @@
+
+export default function Navbar() {
+  return (
+    <nav style={{width:'100%', height:'5rem', backgroundColor:'red'}}>
+      <ul style={{display:'flex', listStyle:'none', height:'100%', alignItems:"center"}}>
+        <li style={{padding:'0 20px 0 5px'}}>Home</li>
+        <li>Products</li>
+      </ul>
+        
+    </nav>
+  )
+}
