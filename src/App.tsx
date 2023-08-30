d939aacd343a8cd9951ece5c3190acea2cc8981f
Aniket Tamrakar <anikettamrakar0@gmail.com>
2023-08-30 13:23:53 +0545

    Home and Cart pages added

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,26 @@
 import Navbar from "./component/Navbar/Navbar";
-import {
-  QueryClient,
-  QueryClientProvider,
-  useQuery,
-} from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Product from "./component/Product/Product";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Cart from "./Pages/Cart";
 
 function App() {
   const client = new QueryClient();
 
   return (
     <div className="App">
-    <QueryClientProvider>
-
-      <Navbar />
-    </QueryClientProvider>
+      <QueryClientProvider client={client}>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/cart" element={<Cart />}></Route>
+          </Routes>
+        </BrowserRouter>
+        <Product />
+      </QueryClientProvider>
     </div>
   );
 }
